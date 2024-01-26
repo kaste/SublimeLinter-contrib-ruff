c9045c9100ce40d4d7b0e3e3edfa134fe3e78a5e
herr kaste <herr.kaste@gmail.com>
2024-01-26 10:14:54 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
-This is a template. For "how to make a linter", please check [the HOWTO](HOWTO.md).
+SublimeLinter-contrib-ruff
+==========================
 
------------------------------------------------------------------
+[![Build Status](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-ruff.svg?branch=master)](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-ruff)
 
-SublimeLinter-contrib-__linter__
-================================
-
-[![Build Status](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-__linter__.svg?branch=master)](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-__linter__)
-
-This linter plugin for [SublimeLinter](https://github.com/SublimeLinter/SublimeLinter) provides an interface to [__linter__](__linter_homepage__). It will be used with files that have the “__syntax__” syntax.
+This linter plugin for [SublimeLinter](https://github.com/SublimeLinter/SublimeLinter) provides an interface to [ruff](https://docs.astral.sh/ruff/). It will be used with files that have the “Python” syntax.
 
 ## Installation
 SublimeLinter must be installed in order to use this plugin. 
 
 Please use [Package Control](https://packagecontrol.io) to install the linter plugin.
 
-Before installing this plugin, you must ensure that `__linter__` is installed on your system.
+Before installing this plugin, you must ensure that `ruff` is installed on your system. Typically
 
-In order for `__linter__` to be executed by SublimeLinter, you must ensure that its path is available to SublimeLinter. The docs cover [troubleshooting PATH configuration](http://sublimelinter.readthedocs.io/en/latest/troubleshooting.html#finding-a-linter-executable).
+```
+pip install ruff
+# or:
+rye install ruff
+```
+
+will do that.  You can also install it into a virtual environment and SublimeLinter will find it.
+
 
 ## Settings
 - SublimeLinter settings: http://sublimelinter.readthedocs.org/en/latest/settings.html
 - Linter settings: http://sublimelinter.readthedocs.org/en/latest/linter_settings.html
 
-Additional SublimeLinter-__linter__ settings:
+Additional SublimeLinter-ruff settings:
 
 |Setting|Description    |
 |:------|:--------------|
-|foo    |Something.     |
-|bar    |Something else.|
+|no-cache                  |Default: `True`.  As this plugin by default runs on every python file, turn the cache off.  `ruff` is already fast without a cache but turn this back on (`false`) on projects.|
+|disable_if_not_dependency |Default: `False`.  If set to `true`, use only locally installed `ruff` executables from virtual environments or skip linting the project.
+|check_for_local_configuration |Set to `true` to check for a local "ruff.toml" configuration file. Skip running ruff if such a file cannot be found.[1]|
+
+
+[1] Unfortunately "pyproject.toml" detection is not implemented as we don't have a toml parser at hand in Python 3.3 (yik, we run 3.3 here). (TODO?)
+
--- linter.py
+++ linter.py
@@ -1,10 +1,88 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+import json
+import logging
+import os
+
+from SublimeLinter.lint import LintMatch, PermanentError, PythonLinter
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
+class Ruff(PythonLinter):
+    cmd = 'ruff check --output-format=json -'
+    regex = None
     defaults = {
-        'selector': 'source.python'
+        "selector": "source.python",
+        # As we run automatically everywhere, turn off the cache to
+        # not create a temp-file-mess.
+        "--no-cache": True,
+        # Check for "ruff.toml" and ".ruff.toml" configuration files
+        # and skip linting otherwise.
+        "check_for_local_configuration": False,
     }
+    config_file_names = ("ruff.toml", ".ruff.toml", )
+
+    def run(self, cmd, code):
+        cwd = self.get_working_dir()
+        if (
+            self.settings.get("check_for_local_configuration")
+            and cwd and not any(
+                os.path.exists(os.path.join(cwd, name))
+                for name in self.config_file_names
+            )
+        ):
+            self.logger.info(
+                "Skip ruff: no local configuration found at '{}'".format(cwd)
+            )
+            self.notify_unassign()
+            raise PermanentError("no local configuration found")
+        return super().run(cmd, code)
+
+    def find_errors(self, output):
+        try:
+            content = json.loads(output)
+        except ValueError:
+            self.logger.error(
+                "JSON Decode error: We expected JSON from 'ruff', "
+                "but instead got this:\n{}\n\n"
+                .format(output)
+            )
+            self.notify_failure()
+            return
+
+        if self.logger.isEnabledFor(logging.INFO):
+            import pprint
+            self.logger.info(
+                '{} output:\n{}'.format(self.name, pprint.pformat(content)))
+
+        """
+          {
+            "cell": null,
+            "code": "E501",
+            "end_location": {
+              "column": 140,
+              "row": 24
+            },
+            "filename": "~\\api.py",
+            "fix": null,
+            "location": {
+              "column": 121,
+              "row": 24
+            },
+            "message": "Line too long (139 > 120)",
+            "noqa_row": 24,
+            "url": "https://docs.astral.sh/ruff/rules/line-too-long"
+          }
+        """
+
+        for match in content:
+            code = match["code"]
+            yield LintMatch(
+                match=match,
+                filename=match["filename"],
+                line=match["location"]["row"] - 1,
+                col=match["location"]["column"] - 1,
+                end_line=match["end_location"]["row"] - 1,
+                end_col=match["end_location"]["column"] - 1,
+                error_type="error" if code.startswith("F") else "warning",
+                code=code,
+                message=match["message"],
+
+            )
